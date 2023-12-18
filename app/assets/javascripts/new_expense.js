acb097e62d59460efc4083873f64b52aa3a0767f
Andrew Njoo <adnjoo@gmail.com>
2023-12-17 17:26:42 -0800

    fix

--- app/assets/javascripts/new_expense.js
+++ app/assets/javascripts/new_expense.js
@@ -10,8 +10,15 @@ document.addEventListener("DOMContentLoaded", function () {
   if (timezoneInput && datetimeField) {
     const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     timezoneInput.value = userTimezone;
+  
+    const now = new Date(); // Get the current date and time
+    const year = now.getFullYear();
+    const month = `${(now.getMonth() + 1)}`.padStart(2, "0"); // Month starts from 0
+    const day = `${now.getDate()}`.padStart(2, "0");
+    const hours = `${now.getHours()}`.padStart(2, "0");
+    const minutes = `${now.getMinutes()}`.padStart(2, "0");
 
-    const now = new Date().toISOString({ timeZone: userTimezone }).slice(0, 16);
-    datetimeField.value = now;
+    const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}`;
+    datetimeField.value = formattedDate;
   }
 });
