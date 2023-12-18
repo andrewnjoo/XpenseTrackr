001074649fa66acfa5a9e8d771a3de97ea3b1a44
Andrew Njoo <adnjoo@gmail.com>
2023-12-17 18:26:02 -0800

    document.readyState

--- app/assets/javascripts/new_expense.js
+++ app/assets/javascripts/new_expense.js
@@ -3,17 +3,16 @@
  * Retrieves the user's timezone and populates a hidden input field with the timezone value.
  * Updates the datetime input field to display the current time in the user's timezone on page load.
  */
-document.addEventListener("DOMContentLoaded", function () {
+function executeWhenDOMLoaded() {
   const datetimeField = document.querySelector("input[type=datetime-local]");
   const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-  console.log('userTimezone', userTimezone);
+  console.log("userTimezone", userTimezone);
 
   if (datetimeField) {
-    const now = new Date(); // Get the current date and time
-    console.log(now);
+    const now = new Date();
     const year = now.getFullYear();
-    const month = `${(now.getMonth() + 1)}`.padStart(2, "0"); // Month starts from 0
+    const month = `${now.getMonth() + 1}`.padStart(2, "0");
     const day = `${now.getDate()}`.padStart(2, "0");
     const hours = `${now.getHours()}`.padStart(2, "0");
     const minutes = `${now.getMinutes()}`.padStart(2, "0");
@@ -21,4 +20,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}`;
     datetimeField.value = formattedDate;
   }
-});
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", executeWhenDOMLoaded);
+} else {
+  executeWhenDOMLoaded();
+}
